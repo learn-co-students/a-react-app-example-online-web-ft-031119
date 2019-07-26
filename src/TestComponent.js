63a1e4898378263ea1b929a05f8d6f0dd62c756b
Katie Wilday <wildayk@gmail.com>
2019-07-26 15:17:49 -0400

    Done.

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -6,6 +6,7 @@ class TestComponent extends Component {
     return (
       <div className="video">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/5TbUxGZtwGI" frameBorder="0" allow="autoplay; encrypted-media" title="time video" allowFullScreen></iframe>
+        <p>Test Component</p>
       </div>
     );
   }
